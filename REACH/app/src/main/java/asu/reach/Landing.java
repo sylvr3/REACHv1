d49bf309241128c8723aa61f57feeb4132a43b48
Derek Hamel <dbhamel@asu.edu>
2015-01-15 16:46:39 -0700

    Initial UI commit

--- REACH/app/src/main/java/asu/reach/Landing.java
+++ REACH/app/src/main/java/asu/reach/Landing.java
@@ -1,19 +1,76 @@
 package asu.reach;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 
-public class Landing extends Activity {
+public class Landing extends Activity implements View.OnClickListener {
+
+    private VideoView vv;
+    private int stopPosition=0;
+    private RelativeLayout dd,stic,stop,vid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_landing);
+
+        if(savedInstanceState != null) {
+            stopPosition = savedInstanceState.getInt("position",0);
+        }
+        vv = (VideoView)findViewById(R.id.videoView);
+        dd = (RelativeLayout)findViewById(R.id.ddLayout);
+        stic = (RelativeLayout)findViewById(R.id.sticLayout);
+        stop = (RelativeLayout)findViewById(R.id.stopLayout);
+        vid = (RelativeLayout)findViewById(R.id.vidLayout);
+
+        vv.setVideoURI(Uri.parse("android.resource://asu.reach/" + R.raw.idle2));
+        vv.requestFocus();
+        vv.start();
+
+        vid.setOnClickListener(this);
+        dd.setOnClickListener(this);
+        stic.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == vid.getId()){
+            Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show();
+        }
+        if(v.getId() == dd.getId()){
+
+            Toast.makeText(this, "DD", Toast.LENGTH_SHORT).show();
+        }
+        if(v.getId() == stic.getId()){
+
+            Toast.makeText(this, "STIC", Toast.LENGTH_SHORT).show();
+        }
+        if(v.getId() == stop.getId()){
+
+            Toast.makeText(this, "STOP", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +89,20 @@ public class Landing extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        vv.seekTo(stopPosition);
+        vv.start();
+        super.onResume();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        stopPosition = vv.getCurrentPosition();
+        vv.pause();
+        outState.putInt("position", stopPosition);
     }
 }
