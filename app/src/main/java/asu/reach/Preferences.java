5fb8bf9cd52dc0ab7ca704dc393cd249bf275f2b
Chinmay Dhekne <chinmaydhekne@gmail.com>
2015-03-02 19:03:34 -0700

    STIC Nested Preferences
    Done

--- app/src/main/java/asu/reach/Preferences.java
+++ app/src/main/java/asu/reach/Preferences.java
@@ -7,18 +7,20 @@ import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import java.util.Arrays;
 import java.util.Set;
 
 public class Preferences extends PreferenceActivity /*implements SharedPreferences.OnSharedPreferenceChangeListener */ {
 
-    private MultiSelectListPreference DDProtocolChange,STOPProtocolChange,STICProtocolChange;
+    private MultiSelectListPreference DDProtocolChange, STOPProtocolChange, STICProtocolChange;
     private SharedPreferences sharedPrefs;
     private SQLiteDatabase db;
     private int i = 0;
@@ -30,16 +32,31 @@ public class Preferences extends PreferenceActivity /*implements SharedPreferenc
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         addPreferencesFromResource(R.xml.prefs);
+        PreferenceManager.setDefaultValues(this, R.xml.prefs, true);
         DDProtocolChange = (MultiSelectListPreference) findPreference("DD_week_setting");
+        STOPProtocolChange = (MultiSelectListPreference) findPreference("STOP_week_setting");
+//        STICProtocol_Week1Change = (ListPreference) findPreference("Week1_STIC_protool_setting");
+//        DDProtocolChange.setva
         SharedPreferences spf = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         spf.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-                if(s.equals("DD_week_setting")){
-                    dailyDiaryProtocolChange(sharedPreferences,s);
-                }
-                else if (s.equals("STOP_week_setting")){
-                    StopProtocolChange(sharedPreferences,s);
+                if (s.equals("DD_week_setting")) {
+                    dailyDiaryProtocolChange(sharedPreferences, s);
+                } else if (s.equals("STOP_week_setting")) {
+                    StopProtocolChange(sharedPreferences, s);
+                } else if (s.equals("Week1_STIC_protool_setting")) {
+                    SticProtocolChange(sharedPreferences, s, 1);
+                } else if (s.equals("Week2_STIC_protool_setting")) {
+                    SticProtocolChange(sharedPreferences, s, 2);
+                } else if (s.equals("Week3_STIC_protool_setting")) {
+                    SticProtocolChange(sharedPreferences, s, 3);
+                } else if (s.equals("Week4_STIC_protool_setting")) {
+                    SticProtocolChange(sharedPreferences, s, 4);
+                } else if (s.equals("Week5_STIC_protool_setting")) {
+                    SticProtocolChange(sharedPreferences, s, 5);
+                } else if (s.equals("Week6_STIC_protool_setting")) {
+                    SticProtocolChange(sharedPreferences, s, 6);
                 }
             }
         });
@@ -56,7 +73,23 @@ public class Preferences extends PreferenceActivity /*implements SharedPreferenc
 */
     }
 
-    public void dailyDiaryProtocolChange(SharedPreferences pref,String s) {
+    public void SticProtocolChange(SharedPreferences pref, String s, int week_no) {
+        try {
+            DBHelper helper = new DBHelper(getApplicationContext());
+            db = helper.getDB();
+            ContentValues v;
+            String listToPopulate = pref.getString("Week" + week_no + "_STIC_protool_setting", null);
+            v = new ContentValues();
+            v.put("STIC", listToPopulate);
+            int noOfRowsUpdated = db.update("ADMIN_ACTIVITY_SCHEDULER", v, "WEEK_NO=" + week_no, null);
+            Log.d("Rows Updated for STIC_WEEK"+week_no+"", noOfRowsUpdated + "");
+            db.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dailyDiaryProtocolChange(SharedPreferences pref, String s) {
         Set<String> stringSet = pref.getStringSet("DD_week_setting", null);
         String[] stringArr = stringSet.toArray(new String[stringSet.size()]);
         String[] stringArrNotContains = new String[6 - stringArr.length];
@@ -95,7 +128,7 @@ public class Preferences extends PreferenceActivity /*implements SharedPreferenc
 
     }
 
-    public void StopProtocolChange(SharedPreferences pref,String s){
+    public void StopProtocolChange(SharedPreferences pref, String s) {
         Set<String> stringSet = pref.getStringSet("STOP_week_setting", null);
         String[] stringArr = stringSet.toArray(new String[stringSet.size()]);
         String[] stringArrNotContains = new String[6 - stringArr.length];
@@ -112,9 +145,9 @@ public class Preferences extends PreferenceActivity /*implements SharedPreferenc
             db = helper.getDB();
             i = 0;
             ContentValues v = new ContentValues();
-            v.put("STOP",1);
+            v.put("STOP", 1);
             while (i < stringArr.length) {
-                db.update("ADMIN_ACTIVITY_SCHEDULER",v,"DAY not in(1,8,15,22,29,36,42) and WEEK_NO = \"" + Integer.parseInt(stringArr[i++].toString())+ "\"",null);
+                db.update("ADMIN_ACTIVITY_SCHEDULER", v, "DAY not in(1,8,15,22,29,36,42) and WEEK_NO = \"" + Integer.parseInt(stringArr[i++].toString()) + "\"", null);
 //                Cursor cursor = db.rawQuery("update ADMIN_ACTIVITY_SCHEDULER set STOP=1 where DAY not in(1,8,15,22,29,36,42) and WEEK_NO=" + Integer.parseInt(stringArr[i].toString()), null);
 //                cursor.close();
 
@@ -124,9 +157,9 @@ public class Preferences extends PreferenceActivity /*implements SharedPreferenc
             db = helper.getDB();
             i = 0;
             ContentValues v1 = new ContentValues();
-            v1.put("STOP",0);
+            v1.put("STOP", 0);
             while (i < stringArrNotContains.length) {
-                db.update("ADMIN_ACTIVITY_SCHEDULER",v1,"WEEK_NO = \"" + Integer.parseInt(stringArrNotContains[i++].toString())+ "\"",null);
+                db.update("ADMIN_ACTIVITY_SCHEDULER", v1, "WEEK_NO = \"" + Integer.parseInt(stringArrNotContains[i++].toString()) + "\"", null);
             }
 
             db.close();
@@ -134,7 +167,6 @@ public class Preferences extends PreferenceActivity /*implements SharedPreferenc
             e.printStackTrace();
         }
     }
-
 
 
 }
