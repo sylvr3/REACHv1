5846f1d74eae88fb4f74edca2921d17c81406fae
Stefano Chang <swchang10@hotmail.com>
2017-01-31 23:06:00 -0700

    add webview first page

---
+++ app/src/main/java/asu/reach/SafeWebView.java
@@ -0,0 +1,35 @@
+package asu.reach;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+public class SafeWebView extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.safe_web_view);
+        mWebView = (WebView) findViewById(R.id.safe_web_view);
+        mWebView.setInitialScale(1);
+        mWebView.setVerticalScrollBarEnabled(false);
+        mWebView.setHorizontalScrollBarEnabled(false);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        mWebView.loadUrl("file:///android_asset/www/html/index.html");
+    }
+}
