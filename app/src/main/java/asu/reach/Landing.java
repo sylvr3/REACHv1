50ed491579b87a459e3be8f37bb97873260b56f0
Stefano Chang <swchang10@hotmail.com>
2017-02-15 17:04:05 -0700

    change deafult safe to native code

--- app/src/main/java/asu/reach/Landing.java
+++ app/src/main/java/asu/reach/Landing.java
@@ -252,7 +252,7 @@ public class Landing extends Activity implements View.OnClickListener,DialogInte
 
         // Safe
         if(v.getId() == safe.getId()) {
-            Intent intent = new Intent(this, SafeWebView.class);
+            Intent intent = new Intent(this, Safe.class);
 
             startActivity(intent);
         }
