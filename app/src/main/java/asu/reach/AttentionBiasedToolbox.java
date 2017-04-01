74e67c5db245aba80a4b383307d674633f6dbc2a
vckadam <vckadam@asu.edu>
2017-03-31 20:18:41 -0700

    Added new session, new timer for response and score

--- app/src/main/java/asu/reach/AttentionBiasedToolbox.java
+++ app/src/main/java/asu/reach/AttentionBiasedToolbox.java
@@ -2,28 +2,24 @@ package asu.reach;
 
 import android.app.Activity;
 import android.content.DialogInterface;
-import android.content.pm.ActivityInfo;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.text.Layout;
 import android.view.View;
-import android.view.Window;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
-import org.w3c.dom.Text;
-
+import java.util.Arrays;
 import java.util.Random;
 
 public class AttentionBiasedToolbox extends Activity implements View.OnClickListener{
@@ -31,20 +27,18 @@ public class AttentionBiasedToolbox extends Activity implements View.OnClickList
     private Random random;
     private TypedArray neutralImgs, threatImgs;
     private Bitmap[] bmap;
-    private int neutral,count, index, numOfTrials;
-    private CountDownTimer countDownTimer, blankScreenTimer;
+    private int neutral,count, index, totalAttempts;
+    private CountDownTimer countDownTimer, blankScreenTimer, responseTimer, transitionTimer;
     private ImageView plusImage;
     private ImageView plusBtwImageView;
     private ViewFlipper viewFlipper;
-    private Button leftButton, rightButton, tutorialButton, trialButton;
+    private Button leftButton, rightButton, restartButton;
     private int[] imageIndArray;
-    private TextView scoreText;
+    private EditText resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_attention_biased_toolbox);
         random = new Random();
         bmap = new Bitmap[2];
@@ -54,33 +48,29 @@ public class AttentionBiasedToolbox extends Activity implements View.OnClickList
         for(int i = 0; i < imageIndArray.length; i++) imageIndArray[i] = i;
         index = 0;
         neutral = 0;
+        totalAttempts = 0;
         imgTop= (ImageView)findViewById(R.id.imgTop);
         imgBottom = (ImageView)findViewById(R.id.imgBottom);
         Typeface font = Typeface.createFromAsset(getAssets(), "agentorange.ttf");
         leftButton = (Button)findViewById(R.id.leftButton);
+        restartButton = (Button)findViewById(R.id.restartButton);
         leftButton.setTypeface(font);
         rightButton = (Button)findViewById(R.id.rightButton);
         rightButton.setTypeface(font);
-        Typeface textType = Typeface.createFromAsset(getAssets(), "Average-Regular.ttf");
-        tutorialButton = (Button)findViewById(R.id.tutorialButton);
-        tutorialButton.setTypeface(textType);
-        trialButton = (Button)findViewById(R.id.trialButton);
-        trialButton.setTypeface(textType);
         plusImage = (ImageView)findViewById(R.id.plus);
         plusBtwImageView = (ImageView) findViewById(R.id.plusBtw);
         viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
-        viewFlipper.showNext();
-        scoreText = (TextView)findViewById(R.id.scoreText);
+        resultText = (EditText)findViewById(R.id.resultText);
+        //viewFlipper.showNext();
         imgTop.setOnClickListener(this);
         imgBottom.setOnClickListener(this);
         leftButton.setOnClickListener(this);
         rightButton.setOnClickListener(this);
-        tutorialButton.setOnClickListener(this);
-        trialButton.setOnClickListener(this);
-        //trialButton.setClickable(false);
+        restartButton.setOnClickListener(this);
         imgTop.setVisibility(View.INVISIBLE);
         imgBottom.setVisibility(View.INVISIBLE);
-        blankScreenTimer = new CountDownTimer(500,200) {
+        restartButton.setVisibility(View.INVISIBLE);
+        responseTimer = new CountDownTimer(200,100) {
             @Override
             public void onTick(long l) {
 
@@ -88,10 +78,32 @@ public class AttentionBiasedToolbox extends Activity implements View.OnClickList
 
             @Override
             public void onFinish() {
+                transitionScreen();
+            }
+        };
+        transitionTimer = new CountDownTimer(1800,900) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                showBlankScreen();
+            }
+        };
+        blankScreenTimer = new CountDownTimer(500,250) {
+            @Override
+            public void onTick(long l) {
+                screenWithBlankImages();
+            }
+
+            @Override
+            public void onFinish() {
                 fetchImages();
             }
         };
-        countDownTimer = new CountDownTimer(200,200) {
+        countDownTimer = new CountDownTimer(500,250) {
 
             public void onTick(long millisUntilFinished) {
 
@@ -102,11 +114,33 @@ public class AttentionBiasedToolbox extends Activity implements View.OnClickList
                 showProbes();
             }
         };
-       // blankScreen();
-        //showBlankScreen();
-        showMainScreen();
+        //blankScreen();
+        showBlankScreen();
     }
 
+    public void transitionScreen() {
+        viewFlipper.setDisplayedChild(1);
+        responseTimer.cancel();
+        transitionTimer.start();
+        imgTop.setVisibility(View.INVISIBLE);
+        imgBottom.setVisibility(View.INVISIBLE);
+        leftButton.setVisibility(View.VISIBLE);
+        rightButton.setVisibility(View.VISIBLE);
+        rightButton.setEnabled(false);
+        leftButton.setEnabled(false);
+    }
+
+    protected void onStop() {
+        super.onStop();
+        countDownTimer.cancel();
+        blankScreenTimer.cancel();
+        responseTimer.cancel();
+        transitionTimer.cancel();
+    }
+    public void screenWithBlankImages() {
+        //System.out.println("+ sign is removed");
+        //plusBtwImageView.setVisibility(View.INVISIBLE);
+    }
     // Initiate timer for first fixation with + sign, at the end it start the fetching images
     public void blankScreen() {
         blankScreenTimer.start();
@@ -119,6 +153,7 @@ public class AttentionBiasedToolbox extends Activity implements View.OnClickList
 
     // Wipe images and generate probes
     public void showProbes() {
+        countDownTimer.cancel();
         int leftProbeInd = R.drawable.left;
         int rightProbeInd = R.drawable.right;
         imgTop.setVisibility(View.INVISIBLE);
@@ -131,86 +166,65 @@ public class AttentionBiasedToolbox extends Activity implements View.OnClickList
         if(neutral == 0) imgTop.setVisibility(View.VISIBLE);
         else imgBottom.setVisibility(View.VISIBLE);
         //viewFlipper.showPrevious();
-      //  countDownTimer.cancel();
-    }
-
-    // Main screen that allows user to select tutorial or trial
-    public void showMainScreen() {
-        tutorialButton.setVisibility(View.VISIBLE);
-        trialButton.setVisibility(View.VISIBLE);
-        plusBtwImageView.setVisibility(View.GONE);
-        imgTop.setVisibility(View.INVISIBLE);
-        imgBottom.setVisibility(View.INVISIBLE);
-        leftButton.setVisibility(View.GONE);
-        rightButton.setVisibility(View.GONE);
-        plusImage.setVisibility(View.INVISIBLE);
-        scoreText.setVisibility(View.INVISIBLE);
+        viewFlipper.setDisplayedChild(1);
+        responseTimer.start();
     }
 
     //Wipe images and show the + sign
     public void showBlankScreen() {
-        //countDownTimer.cancel();
-        tutorialButton.setVisibility(View.INVISIBLE);
-        trialButton.setVisibility(View.INVISIBLE);
+        restartButton.setVisibility(View.INVISIBLE);
+        countDownTimer.cancel();
+        responseTimer.cancel();
+        transitionTimer.cancel();
         imgTop.setVisibility(View.INVISIBLE);
         imgBottom.setVisibility(View.INVISIBLE);
         leftButton.setVisibility(View.INVISIBLE);
         rightButton.setVisibility(View.INVISIBLE);
-        viewFlipper.showNext();
+        //viewFlipper.showNext();
+        viewFlipper.setDisplayedChild(0);
         plusImage.setVisibility(View.VISIBLE);
-        scoreText.setVisibility(View.INVISIBLE);
         blankScreen();
-    }
-
-    // Display student's score and speed
-    public void showScoreScreen() {
-        tutorialButton.setVisibility(View.INVISIBLE);
-        trialButton.setVisibility(View.INVISIBLE);
-        imgTop.setVisibility(View.INVISIBLE);
-        imgBottom.setVisibility(View.INVISIBLE);
-        leftButton.setVisibility(View.GONE);
-        rightButton.setVisibility(View.GONE);
-        viewFlipper.showNext();
-        plusImage.setVisibility(View.GONE);
-        plusBtwImageView.setVisibility(View.GONE);
-        scoreText.setVisibility(View.VISIBLE);
-        scoreText.setText("Score: " + count + " out of " + numOfTrials);
-        System.out.println("Score: " + count + " out of " + numOfTrials);
-        // todo: figure out how to calculate and display speed
 
     }
 
     //Load images
     public void fetchImages() {
-        if (numOfTrials < 160) {
-            int bitMapInd = random.nextInt(2);
-            int rndInt = random.nextInt(neutralImgs.length() - index) + index;
-            int neutralId = neutralImgs.getResourceId(rndInt, 0);
-            int threatId = threatImgs.getResourceId(rndInt, 0);
-            swapIndex(index, rndInt);
-            index = (index + 1) % neutralImgs.length();
-            bmap[0] = (bitMapInd == 0) ? BitmapFactory.decodeResource(getResources(), neutralId) : BitmapFactory.decodeResource(getResources(), threatId);
-            bmap[1] = (bitMapInd == 1) ? BitmapFactory.decodeResource(getResources(), neutralId) : BitmapFactory.decodeResource(getResources(), threatId);
-            neutral = (bitMapInd == 0) ? 0 : 1;
-            viewFlipper.showPrevious();
-            imgTop.setVisibility(View.VISIBLE);
-            plusBtwImageView.setVisibility(View.VISIBLE);
-            imgBottom.setVisibility(View.VISIBLE);
-            leftButton.setVisibility(View.VISIBLE);
-            rightButton.setVisibility(View.VISIBLE);
-            plusImage.setVisibility(View.INVISIBLE);
-            imgTop.setImageBitmap(bmap[0]);
-            imgBottom.setImageBitmap(bmap[1]);
-            blankScreenTimer.cancel();
-            startTimer();
-        }
-        if (numOfTrials == 160) { // trial is over and score is presented
-               showScoreScreen();
-            }
-            if (count == 160)
-                System.out.println("Trial over");
+        blankScreenTimer.cancel();
+        int bitMapInd = random.nextInt(2);
+        int rndInt = random.nextInt(neutralImgs.length()-index) + index;
+        int neutralId = neutralImgs.getResourceId(imageIndArray[rndInt],0);
+        int threatId = threatImgs.getResourceId(imageIndArray[rndInt],0);
+        swapIndex(index,rndInt);
+        index = (index + 1)%neutralImgs.length();
+        System.out.println(Arrays.toString(imageIndArray));
+        bmap[0] = (bitMapInd == 0)? BitmapFactory.decodeResource(getResources(),neutralId):BitmapFactory.decodeResource(getResources(),threatId);
+        bmap[1] = (bitMapInd == 1)? BitmapFactory.decodeResource(getResources(),neutralId):BitmapFactory.decodeResource(getResources(),threatId);
+        neutral = (bitMapInd == 0)?0:1;
+        //viewFlipper.showPrevious();
+        viewFlipper.setDisplayedChild(1);
+        imgTop.setVisibility(View.VISIBLE);
+        plusBtwImageView.setVisibility(View.VISIBLE);
+        imgBottom.setVisibility(View.VISIBLE);
+        rightButton.setEnabled(true);
+        leftButton.setEnabled(true);
+        leftButton.setVisibility(View.VISIBLE);
+        rightButton.setVisibility(View.VISIBLE);
+        plusImage.setVisibility(View.INVISIBLE);
+        imgTop.setImageBitmap(bmap[0]);
+        imgBottom.setImageBitmap(bmap[1]);
+        totalAttempts++;
+        startTimer();
     }
 
+    public void setUpAgain() {
+        countDownTimer.cancel();
+        blankScreenTimer.cancel();
+        responseTimer.cancel();
+        transitionTimer.cancel();
+        //viewFlipper.showNext();
+        restartButton.setVisibility(View.VISIBLE);
+
+    }
     //Swap the indices to populate the images which is not generated in previous hits.
     public void swapIndex(int i, int j) {
         int temp = imageIndArray[i];
@@ -218,47 +232,38 @@ public class AttentionBiasedToolbox extends Activity implements View.OnClickList
         imageIndArray[j] = temp;
     }
 
-
-
     @Override
     public void onClick(View v) {
-        if(v.getId() == tutorialButton.getId()) {
-            tutorialButton.setVisibility(View.INVISIBLE);
-            trialButton.setVisibility(View.INVISIBLE);
-        }
-
-        if(v.getId() == trialButton.getId()) {
-            tutorialButton.setVisibility(View.INVISIBLE);
-            trialButton.setVisibility(View.INVISIBLE);
-            blankScreen();
+        /*if(v.getId() == imgTop.getId()) {
+            if(neutral == 0) count++;
             showBlankScreen();
         }
-
+        if(v.getId() == imgBottom.getId()) {
+            if(neutral == 1) count++;
+            showBlankScreen();
+        }*/
         if(v.getId() == leftButton.getId()) {
-            if(neutral == 0) {
-                count++;
-                MediaPlayer mp = MediaPlayer.create(this, R.raw.ding);;
-                mp.start();
-               // mp.release();
-            }
-            numOfTrials++;
-            showBlankScreen();
+            if(neutral == 0 ) count++;
+            //showBlankScreen();
         }
         if(v.getId() == rightButton.getId()) {
-            if(neutral == 1) {
-                count++;
-                MediaPlayer mp = MediaPlayer.create(this, R.raw.ding);;
-                mp.start();
-               // mp.release();
-            }
-            numOfTrials++;
+            if(neutral == 1) count++;
+            //showBlankScreen();
+        }
+        if(v.getId() == restartButton.getId()) {
+            //viewFlipper.showNext();
+            //viewFlipper.showPrevious();
+            totalAttempts = 0;
             showBlankScreen();
 
         }
-
-
+        if(count == 5) {
+            count = 0;
+            viewFlipper.setDisplayedChild(2);
+            String result = "Score: " + totalAttempts;
+            resultText.setText(result);
+            setUpAgain();
+        }
         System.out.println(count);
-        System.out.println(numOfTrials);
-
     }
 }
