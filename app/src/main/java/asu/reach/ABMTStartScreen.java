abbd0c47dba2de9209763f3e8bd60b730515b4a1
Satyam Jaiswal <snjaiswa@asu.edu>
2017-04-11 21:04:43 -0700

    abmt: startup background, removed title

--- app/src/main/java/asu/reach/ABMTStartScreen.java
+++ app/src/main/java/asu/reach/ABMTStartScreen.java
@@ -2,9 +2,11 @@ package asu.reach;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class ABMTStartScreen extends Activity implements View.OnClickListener{
@@ -13,6 +15,8 @@ public class ABMTStartScreen extends Activity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_abmtstart_screen);
         trialButton = (Button)findViewById(R.id.trialButton);
         abmtButton = (Button)findViewById(R.id.abmtButton);
